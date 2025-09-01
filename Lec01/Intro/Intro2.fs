db4dc9209ae4dc9992f60ba8e28340bf42d5749c
AVNBuelow <alby@itu.dk>
2025-09-01 15:51:35 +0200

    finished 1.1, partly completed 1.2

--- Lec01/Intro/Intro2.fs
+++ Lec01/Intro/Intro2.fs
@@ -20,10 +20,12 @@ let cvalue = lookup env "c";;
 
 (* Object language expressions with variables *)
 
+//Extended in accordance to 1.1 (iv)
 type expr = 
   | CstI of int
   | Var of string
   | Prim of string * expr * expr;;
+  | If if expr * expr * expr
 
 let e1 = CstI 17;;
 
@@ -54,7 +56,7 @@ let rec evalOrig e (env : (string * int) list) : int =
     | Prim("==", e1, e2) -> if eval e1 env == eval e2 env then 1 else 0
     | Prim _            -> failwith "unknown primitive";;
 
-//The eval function rewritten after 1.1 (iii)
+//The eval function rewritten after 1.1 (iii) and extended for 1.1 (v)
 let rec eval e (env : (string * int) list) : int =
     match e with
     | CstI i            -> i
@@ -70,6 +72,7 @@ let rec eval e (env : (string * int) list) : int =
         | "min" -> if i1 < i2 then i1 else i2
         | "==" -> if i1 == i2 then 1 else 0
         | _ -> failwith "unknown primitive";;
+    | If(e1, e2, e3)    -> if eval e1 env <> 0 then eval e2 env else eval e3 env
         
 
 let e1v  = eval e1 env;;
@@ -83,3 +86,58 @@ let eMax = eval e4 [("a", 314)];;
 let eMin = eval e5 [("a", 314)];;
 
 let eEquals = eval e6 [("a", 314)];;
+
+// added in 1.2 (i)
+type aexpr = 
+  | CstI of int
+  | Var of string
+  | Add of aexpr * aexpr
+  | Mul of aexpr * aexpr
+  | Sub of aexpr * aexpr
+
+// added for 1.2 (ii)
+let ae1 = Sub("v", Add(Var "w",Var "z"))
+
+let ae2 = Mul(CstI 2, ae1)
+
+let ae3 = Add(Add(Var "x", Var "y"), Add(Var "z", Var "v"))
+
+// added for 1.2 (iii)
+let rec fmt (a: aexpr) : string =
+    match a with
+    | CstI i -> sprintf "%A" i
+    | Var x -> x
+    | Add (a1,a2) -> sprintf "(%A + %A)" (fmt a1) (fmt a2)
+    | Mul (a1, a2) -> sprintf "(%A * %A)" (fmt a1) (fmt a2)
+    | Sub (a1, a2) -> sprintf "(%A - %A)" (fmt a1) (fmt a2)
+
+// added for 1.2 (iv)
+let rec simplify (a: aexpr) : aexpr =
+    match a with
+    | Add (a1,a2) ->
+        let e1 = simplify a1
+        let e2 = simplify a2
+        match e1, e2 with
+        | CstI 0, e2 -> e2
+        | e1, CstI 0 -> e1
+        | _ -> a
+    | Mul (a1, a2) ->
+        let e1 = simplify a1
+        let e2 = simplify a2
+        match e1, e2 with
+        | CstI 1, e2 -> e2
+        | e1, CstI 1 -> e1
+        | CstI 0, e2 -> CstI 0
+        | e1, CstI 0 -> CstI 0
+        | _ -> a
+    | Sub (a1, a2) ->
+        let e1 = simplify a1
+        let e2 = simplify a2
+        match e1, e2 with
+        | e1, CstI 0 -> e1
+        | e1, e2 when e1 == e2 -> CstI 0
+        | _ -> a
+    | _ -> a
+
+// added for 1.2 (v)
+
