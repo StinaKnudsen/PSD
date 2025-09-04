5f5c323225905f2aece679477797f3eaec4ffdf1
Kevin <kegr@itu.dk>
2025-09-04 11:45:49 +0200

    half of assignment 2

--- Lec02/ex2_4Handout.fs
+++ Lec02/ex2_4Handout.fs
@@ -1,17 +1,27 @@
 
 
+
+type sinstr =
+    | SCstI of int  (* push integer *)
+    | SVar of int   (* push variable from env *)
+    | SAdd          (* pop args, push sum *)
+    | SSub          (* pop args, push diff. *)
+    | SMul          (* pop args, push product *)
+    | SPop          (* pop value/unbind var *)
+    | SSwap         (* exchange top and next *)
+
 (* Ex 2.4 - assemble to integers *)
 (* SCST = 0, SVAR = 1, SADD = 2, SSUB = 3, SMUL = 4, SPOP = 5, SSWAP = 6; *)
 let sinstrToInt = function
-  | SCstI i -> [0;i]
-  | SVar i  -> failwith "Not implemented"
-  | SAdd    -> failwith "Not implemented"
-  | SSub    -> failwith "Not implemented"
-  | SMul    -> failwith "Not implemented"
-  | SPop    -> failwith "Not implemented"
-  | SSwap   -> failwith "Not implemented"
+    | SCstI i -> [0;i]
+    | SVar i  -> [1;i]
+    | SAdd    -> [2]
+    | SSub    -> [3]
+    | SMul    -> [4]
+    | SPop    -> [5]
+    | SSwap   -> [6]
 
-let assemble instrs = failwith "Not implemented"
+let assemble (instrs : sinstr list) : int list = List.fold (fun acc elem -> acc @ (sinstrToInt elem)) [] instrs
 
 (* Output the integers in list inss to the text file called fname: *)
 
---
+++ Lec02/exercise3_2.md
@@ -0,0 +1,5 @@
+# Exercise 3.2
+We have constructed the following regular expression:
+
+```(?:a?b+a?)+|a```
+
